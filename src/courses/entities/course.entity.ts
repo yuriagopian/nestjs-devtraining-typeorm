6f4aebb70ad087fa0b899cc56847eaf28c7d5631
yuriagopian <yuri.agopian@hotmail.com>
2022-12-05 11:24:14 -0300

    feat: add course entity typeorm configuration

--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,7 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('courses')
 export class Course {
-    id?: number;
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
     name: string;
+    @Column()
     description: string;
+    @Column('json', { nullable: true })
     tags: string[];
+
+    @Column()
     price: number;
 }
