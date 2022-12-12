16edcce55f8b613ed3e5162f63dbc79960823efd
yuriagopian <yuri.agopian@hotmail.com>
2022-12-12 09:42:20 -0300

    test: Creating test db'

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,45 +1,54 @@
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { CoursesService } from './courses.service';
-import { Body, Controller, Get, HttpCode, Param, Post, HttpStatus, Res, Patch, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  HttpStatus,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
-    constructor(private readonly coursesService: CoursesService) { }
+  constructor(private readonly coursesService: CoursesService) {}
 
-    @Get()
-    findAll() {
-        return this.coursesService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.coursesService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.coursesService.findOne(id)
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coursesService.findOne(id);
+  }
 
-    @Post()
-    create(@Body() createCourseDto: CreateCourseDto) {
-        return this.coursesService.create(createCourseDto)
-    }
+  @Post()
+  create(@Body() createCourseDto: CreateCourseDto) {
+    return this.coursesService.create(createCourseDto);
+  }
 
-    // desestrutura/limita a resposta por um parametro que é deifinido no decorator body
-    @Post('name')
-    // @HttpCode(204)
-    //ou
-    // Enum com todos os status code definidos no http util quando o status code é estatico
-    @HttpCode(HttpStatus.NO_CONTENT)
-    createName(@Body('name') data) {
-        return this.coursesService.create(data)
-    }
+  // desestrutura/limita a resposta por um parametro que é deifinido no decorator body
+  @Post('name')
+  // @HttpCode(204)
+  //ou
+  // Enum com todos os status code definidos no http util quando o status code é estatico
+  @HttpCode(HttpStatus.NO_CONTENT)
+  createName(@Body('name') data) {
+    return this.coursesService.create(data);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateCourseDTO: UpdateCourseDto) {
-        return this.coursesService.update(id, updateCourseDTO)
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCourseDTO: UpdateCourseDto) {
+    return this.coursesService.update(id, updateCourseDTO);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.coursesService.delete(id);
-    }
-
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coursesService.delete(id);
+  }
 }
