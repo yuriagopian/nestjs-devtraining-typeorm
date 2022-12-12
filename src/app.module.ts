c94a8fae7ac1b84b76ee2b9fc88d1e89ad1f2961
yuriagopian <yuri.agopian@hotmail.com>
2022-12-12 09:48:49 -0300

    test: Creating test test e2e scripts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,21 @@ import { CoursesModule } from './courses/courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoursesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: "db", // docker || local || "localhost" || "db"
-    port: 5432,
-    username: "postgres",
-    password: "docker",
-    database: "cursonestjs", // docker || local|| "postgres" || "cursonestjs"
-    autoLoadEntities: false,
-    synchronize: false, // false to production, true to local
-    entities: [__dirname + '/**/*.entity.ts']
-  })],
-  controllers: [AppController,],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db', // docker || local || "localhost" || "db"
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'cursonestjs', // docker || local|| "postgres" || "cursonestjs"
+      autoLoadEntities: false,
+      synchronize: false, // false to production, true to local
+      entities: [__dirname + '/**/*.entity.ts'],
+    }),
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- test/courses/courses.e2e-spec.ts
+++ test/courses/courses.e2e-spec.ts
@@ -2,13 +2,27 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { CoursesModule } from '../../src/courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('Courses: /courses (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CoursesModule],
+      imports: [
+        CoursesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost', // docker || local || "localhost" || "db"
+          port: 5433,
+          username: 'postgres',
+          password: 'docker',
+          database: 'testdb', // docker || local|| "postgres" || "cursonestjs"
+          autoLoadEntities: true,
+          synchronize: true, // false to production, true to local
+          entities: [__dirname + '/**/*.entity.ts'],
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
