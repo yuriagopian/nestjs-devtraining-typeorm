fb62f348736ee922eac2f14b684ba63d5bcf7dad
yuriagopian <yuri.agopian@hotmail.com>
2022-12-13 08:23:31 -0300

    refact: Use Data Source provider

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 
 const configService = new ConfigService();
 import { config } from 'dotenv';
+import { CreateCoursesTable1670369515433 } from './migrations/1670369515433-CreateCoursesTable';
 
 config();
 
@@ -12,18 +13,31 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: configService.get('DB_HOST'), // local -> 'localhost'
+        host: configService.get('DB_HOST'), // local -> 'localhost' 'localhost' ||
         port: configService.get('DB_PORT'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DATABASE_NAME'),
         entities: [__dirname + '/../**/*.entity.js'],
-        // migrations: [__dirname + '/src/migrations/*.ts'],
         migrations: [__dirname + '/src/migrations/*.ts'],
-        synchronize: true,
+        // migrations: [__dirname + '/migrations/*.js'],
+        synchronize: false,
       });
 
       return dataSource.initialize();
     },
   },
 ];
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get('DB_HOST'), // local -> 'localhost' 'localhost' ||
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
+  entities: [__dirname + '/../**/*.entity.js'],
+  // migrations: [__dirname + '/src/migrations/*.ts'],
+  migrations: [CreateCoursesTable1670369515433],
+  synchronize: true,
+});
---
+++ src/migrations/1670875739590-CourseRefactoringTest.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCoursesTable1670369515433 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'courses',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('courses');
+  }
+}
